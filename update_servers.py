9995e8adf50caa354f2fa1515020be343ed1d143
Blizard_Wizard <45040436+BlizardWizard@users.noreply.github.com>
2018-11-14 10:46:06 -0800

    Update update_servers.py

--- update_servers.py
+++ update_servers.py
@@ -1,2 +1,7 @@
 def update_servers():
-  
+  server_list = []
+  server_list_file = open("server_list.txt", "r")
+  for line in server_list_file.readlines():
+    server_list.append(line.replace('\n', ''))
+  server_list_file.close()
+  return server_list
