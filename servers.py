a4fd5595ab900ba515b4fa3ffc0a412a5e46192f
Blizard_Wizard <45040436+BlizardWizard@users.noreply.github.com>
2018-11-14 13:35:00 -0800

    Made update servers meathod

--- servers.py
+++ servers.py
@@ -1,7 +1,9 @@
-def update_servers():
+def get_servers():
   server_list = []
   server_list_file = open("server_list.txt", "r")
   for line in server_list_file.readlines():
     server_list.append(line.replace('\n', ''))
   server_list_file.close()
   return server_list
+def update_servers():
+  open("serrver_list.txt", "w")
